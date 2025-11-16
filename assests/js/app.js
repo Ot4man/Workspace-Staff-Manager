0753757b3590402674c4af98f7badc2cc58282d1
Ot4man <melloukiotmane@gmail.com>
2025-11-16 20:03:54 +0100

    Update Add Worker popup with role selection, photo preview, and experience section; add JS for live image preview.

--- assests/js/app.js
+++ assests/js/app.js
@@ -2,10 +2,13 @@ const addworkerpopup =document.getElementById("addworkerpopup");
 const closepopup =document.getElementById("closepopup");
 const submitworker =document.getElementById("submitworker");
 const addWorkerBtn =document.getElementById("addWorkerBtn");
-
+/////////////
+const inputurl = document.getElementById("urlimage");
+const txtpreview = document.getElementById("txtpreview");
+const imagepreview = document.getElementById("image");
 
 ///////////////////
-
+// popup buttons : add worker ,close 
 addWorkerBtn.addEventListener("click",() => {
     addworkerpopup.classList.remove("hidden");
 });
@@ -17,6 +20,21 @@ addworkerpopup.addEventListener("click", (e) => {
         addworkerpopup.classList.add("hidden");
     }
 });
+//showing image function
+inputurl.addEventListener("input",showimage);
+function showimage(){
+    let url = inputurl.value;
+    if(url !== ""){
+        imagepreview.src = url;
+        imagepreview.classList.remove("hidden");
+        txtpreview.classList.add("hidden")
+    }else{
+        imagepreview.classList.add("hidden");
+        txtpreview.classList.remove("hidden");
+    }
+}
+
+
 
 
 
