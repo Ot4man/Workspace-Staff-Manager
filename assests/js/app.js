5a59443a398697579a87f5c018e78d7d1bf40622
Ot4man <melloukiotmane@gmail.com>
2025-11-21 01:42:44 +0100

    feat(workers): render assigned/unassigned workers + add delete & profile view

--- assests/js/app.js
+++ assests/js/app.js
@@ -189,52 +189,76 @@ function addworker(e) {
 
 }
 
+const zoneRules = {
+    "reception-room": ["receptionest", "manager"],
+    "server-room": ["itguy", "manager"],
+    "security-room": ["security", "manager"],
+    "archives-room": ["manager"],
+    "staff-room": ["manager", "cleaning"],
+    "conference-room": "all"
+};
+//workers zone
+let zoneWorkers = {
+    "reception-room": [],
+    "server-room": [],
+    "security-room": [],
+    "archives-room": [],
+    "staff-room": [],
+    "conference-room": []
+
+};
+
+
+
 //////////////
 // rend profile pop
-function renderWorker(worker) {
-    const unassigneddiv = document.createElement("div");
-    unassigneddiv.className = "unassigneddiv cursor-pointer flex justify-between items-center bg-green-500 border border-gray-200 p-3 rounded-lg";
-
-    // store all data in dataset
-    unassigneddiv.dataset.id = worker.id;
-    unassigneddiv.dataset.name = worker.name;
-    unassigneddiv.dataset.role = worker.role;
-    unassigneddiv.dataset.email = worker.email;
-    unassigneddiv.dataset.phone = worker.phone;
-    unassigneddiv.dataset.image = worker.image;
-    unassigneddiv.dataset.experiences = JSON.stringify(worker.experiences);
-
-    unassigneddiv.innerHTML = `
-        <div class="flex items-center gap-4">
-            <img src="${worker.image}" class="w-20 h-20 rounded-full object-cover border border-gray-300">
+function renderWorker() {
+    // render pour chaque zone
+    const zones=["reception-room","server-room","security-room","archives-room","staff-room","conference-room"];
+    const filterZoneNull = workers.filter(worker => worker.zoneId === null);
+    zones.forEach(zone =>{ 
+        const divzone = document.querySelector("."+zone);
+        const filterworkers = workers.filter(worker => worker.zoneId === zone);
+        filterworkers.forEach(w =>{
+            divzone.innerHTML +=`
+            <div class="flex items-center gap-4">
+            <img src="${w.image}" class="w-20 h-20 rounded-full object-cover border border-gray-300">
             <div>
-                <p class="text-xl font-bold"> Name: ${worker.name}</p>
-                <p class="text-sm"> Role: ${worker.role}</p>
-                <p class="text-sm"> Company: ${worker.company}</p>
+                <p class="text-xl text-center font-bold"> ${w.name}</p>
+                <p class="text-l text-center font-bold">  ${w.role}</p>
             </div>
         </div>
-        <button class="text-red-700 font-bold text-xl deleteBtn">X</button>
-    `;
+        <button class="text-red-700 font-bold text-xl">X</button>
+            `;
 
-    // Delete worker
-    unassigneddiv.querySelector(".deleteBtn").addEventListener("click", () => {
-        deleteWorker(worker.id);
-        unassigneddiv.remove();
+        });
+
     });
 
-    // Show profile
-    unassigneddiv.addEventListener("click", (e) => {
-        if (!e.target.classList.contains("deleteBtn")) {
-            showProfile(unassigneddiv);
-        }
-    });
+    unassignedList.innerHTML = "";
+    filterZoneNull.forEach(membernull=>{
+        unassignedList.innerHTML += `
+            <div class="flex items-center gap-4 unassigneddiv cursor-pointer flex justify-between items-center bg-green-500 border border-gray-200 p-3 rounded-lg" data-id=${membernull.id} >
+                <img src="${membernull.image}" class="w-20 h-20 rounded-full object-cover border border-gray-300" onclick="showProfile(this)">
+                <div>
+                    <p class="text-xl text-center font-bold"> ${membernull.name}</p>
+                    <p class="text-l text-center font-bold">  ${membernull.role}</p>
+                </div>
+                <button class="text-red-700 font-bold text-xl deleteBtn" onclick="deleteWorker(${membernull.id})">X</button>
+            </div>
+        `;
+    })
 
-    unassignedList.appendChild(unassigneddiv);
+
+
 }
+
 
 function deleteWorker(id) {
     workers = workers.filter(wor => wor.id !== id);
     saveWorkers();
+    renderWorker()
+
 }
 
 //////////////
@@ -246,46 +270,43 @@ window.addEventListener("DOMContentLoaded", () => {
 //////////////
 // show profile
 function showProfile(card) {
-    let experienceshtml = "";
-    let expers = card.dataset.experiences;
-    if (expers) {
-        expers = JSON.parse(expers);
-    } else {
-        expers = [];
-    }
-
-    expers.forEach(exp => {
-        experienceshtml += `
-            <div class="bg-gray-100 p-4 rounded-lg space-y-2">
-                <p><strong>Company:</strong> ${exp.company}</p>
-                <p><strong>Role:</strong> ${exp.role}</p>
-                <p><strong>Period:</strong> ${exp.from} - ${exp.to}</p>
-            </div>
-        `;
-    });
-    //injection of html 
-    profileContent.innerHTML = `
-        <h2 class="text-3xl font-bold text-center mb-6">Profile</h2>
-        <div class="flex gap-6 items-center">
-            <img src="${card.dataset.image}" class="w-32 h-32 rounded-full object-cover border shadow">
-            <div>
-                <p class="text-2xl font-bold">${card.dataset.name}</p>
-                <p class="text-gray-700 text-lg">${card.dataset.role}</p>
-            </div>
-        </div>
-
-        <div class="bg-gray-100 p-4 rounded-lg space-y-2">
-            <p><strong>Email:</strong> ${card.dataset.email}</p>
-            <p><strong>Phone:</strong> ${card.dataset.phone}</p>
-        </div>
-
-        <h3 class="text-2xl font-bold mt-6">Work Experience :</h3>
-        ${experienceshtml}
-
-        <button class="text-red-700 font-bold text-xl editbtn">Edit</button>
-    `;
-
-    workerProfile.classList.remove("hidden");
+    const  id = card.closest(".unassigneddiv").dataset.id
+    const filtermemeber = workers.find(worker=>Number(worker.id) === Number(id))
+             let experienceshtml = "";
+             filtermemeber.experiences.forEach(exp => {
+                 experienceshtml += `
+                     <div class="bg-gray-100 p-4 rounded-lg space-y-2">
+                         <p><strong>Company:</strong> ${exp.company}</p>
+                         <p><strong>Role:</strong> ${exp.role}</p>
+                         <p><strong>Period:</strong> ${exp.from} - ${exp.to}</p>
+                     </div>
+                 `;
+             });
+             //injection of html 
+             profileContent.innerHTML = `
+                 <h2 class="text-3xl font-bold text-center mb-6">Profile</h2>
+                 <div class="flex gap-6 items-center">
+                     <img src="${filtermemeber.image}" class="w-32 h-32 rounded-full object-cover border shadow">
+                     <div>
+                         <p class="text-2xl font-bold">${filtermemeber.name}</p>
+                         <p class="text-gray-700 text-lg">${filtermemeber.role}</p>
+                     </div>
+                 </div>
+         
+                 <div class="bg-gray-100 p-4 rounded-lg space-y-2">
+                     <p><strong>Email:</strong> ${filtermemeber.email}</p>
+                     <p><strong>Phone:</strong> ${filtermemeber.phone}</p>
+                 </div>
+         
+                 <h3 class="text-2xl font-bold mt-6">Work Experience :</h3>
+                 ${experienceshtml}
+         
+                 <button class="text-red-700 font-bold text-xl editbtn">Edit</button>
+             `;
+         
+             workerProfile.classList.remove("hidden");
+        
+        
 }
 
 
@@ -345,125 +366,6 @@ function getexperiences() {
 
     return exp;
 }
-/////////zone functiions 
-//rule of each room
-const zoneRules = {
-    "reception-room": ["receptionest", "manager"],
-    "server-room": ["itguy", "manager"],
-    "security-room": ["security", "manager"],
-    "archives-room": ["manager"],
-    "staff-room": ["manager", "cleaning"],
-    "conference-room": "all"
-};
-//workers zone
-let zoneWorkers = {
-    "reception-room": [],
-    "server-room": [],
-    "security-room": [],
-    "archives-room": [],
-    "staff-room": [],
-    "conference-room": []
-
-};
-// let unassignedWorkers =[];
-workers.forEach(w => {
-    if (w.zoneId) zoneWorkers[w.zoneId].push(w);
-});
-function assignWorkerToZone(worker, zone) {
-    const allowedRoles = zoneRules[zone];
-
-    if (allowedRoles !== "all" && !allowedRoles.includes(worker.role)) {
-        alert(`${worker.name} cant be assigned to ${zone}`);
-        return false;
-    }
-    worker.zoneId = zone;
-    zoneWorkers[zone].push(worker);
-
-    unassignedWorkers = unassignedWorkers.filter(w => w.id !== worker.id);
-    return true;
-}
-
-const assignPopup = document.getElementById("assignPopup");
-const assignRoomName = document.getElementById("assignRoomName");
-const assignWorkersList = document.getElementById("assignWorkersList");
-const assignCancel = document.getElementById("assignCancel");
 
 
-
-
-let currentZone = null;
-
-// Open popup 
-document.querySelectorAll(".add-btn").forEach(btn => {
-    btn.addEventListener("click", () => {
-        currentZone = btn.dataset.zone;
-
-        assignWorkersList.innerHTML = "";
-        if (unassignedWorkers.length === 0) {
-            assignWorkersList.innerHTML = ` <div class="bg-white p-8 w-11/12 md:w-3/4 max-w-3xl rounded shadow-lg relative max-h-[90vh] overflow-y-auto ">
-                                            <p class='text-gray-500'>No unassigned workers</p>            
-                                            </div>`
-        } else {
-            unassignedWorkers.forEach(worker => {
-                const workerBtn = document.createElement("button");
-                // workerBtn.className = "w-full text-left px-3 py-2 bg-blue-500 text-white rounded hover:bg-blue-600";
-                // workerBtn.textContent = ` smiya: ${worker.name} lkhdma: (${worker.role})`;
-                workerBtn.innerHTML = `<div class ="bg-white p-8 w-11/12 md:w-3/4 max-w-3xl rounded shadow-lg relative max-h-[90vh] overflow-y-auto ">
-                <p> Name: ${worker.name} Role: (${worker.role})</p>
-                </div>
-`;
-
-                workerBtn.addEventListener("click", () => {
-                    if (assignWorkerToZone(worker, currentZone)) {
-                        assignPopup.classList.add("hidden");
-                        saveWorkers();
-                        renderWorkersInRoom(currentZone);
-                    }
-                });
-                assignWorkersList.appendChild(workerBtn);
-            });
-        }
-
-        assignPopup.classList.remove("hidden");
-    });
-});
-
-// Close popup
-assignCancel.addEventListener("click", () => assignPopup.classList.add("hidden"));
-assignPopup.addEventListener("click", e => {
-    if (e.target === assignPopup) assignPopup.classList.add("hidden");
-});
-
-
-for (let zone in zoneWorkers) renderWorkersInRoom(zone);
-
-
-function renderWorkersInRoom(zone) {
-    const zoneDiv = document.getElementById(zone).querySelector(".zone-content");
-    zoneDiv.innerHTML = "";
-
-    zoneWorkers[zone].forEach(worker => {
-
-        const div = document.createElement("div");
-        div.innerHTML = `
-            <div class="bg-white p-8 rounded shadow flex justify-between items-center">
-                <p>${worker.name} (${worker.role})</p>
-                <button class="deletfromzon bg-red-500 text-white px-3 py-1 rounded">x</button>
-            </div>
-        `;
-
-        const deleteBtn = div.querySelector(".deletfromzon");
-        deleteBtn.addEventListener("click", () => {
-
-            zoneWorkers[zone] = zoneWorkers[zone].filter(w => w.id !== worker.id);
-            worker.zoneId = null;
-            unassignedWorkers.push(worker);
-
-            saveWorkers();
-
-            renderWorkersInRoom(zone);
-        });
-
-        zoneDiv.appendChild(div);
-    });
-}
+renderWorker()
