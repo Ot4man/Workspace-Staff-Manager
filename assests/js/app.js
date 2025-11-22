69d7716837be4f41b05cdc22fd81d5c9ede6d0d9
Ot4man <melloukiotmane@gmail.com>
2025-11-22 07:55:14 +0100

    feat(assignment): Ass unassigned button and update

--- assests/js/app.js
+++ assests/js/app.js
@@ -197,14 +197,14 @@ function addworker(e) {
 // rend profile pop
 function renderWorker() {
     // render pour chaque zone
-    const zones=["reception-room","server-room","security-room","archives-room","staff-room","conference-room"];
+    const zones = ["reception-room", "server-room", "security-room", "archives-room", "staff-room", "conference-room"];
     const filterZoneNull = workers.filter(worker => worker.zoneId === null);
-    zones.forEach(zone =>{ 
+    zones.forEach(zone => {
         const divzone = document.getElementById(zone);
         const filterworkers = workers.filter(worker => worker.zoneId === zone);
         divzone.innerHTML = ""
-        filterworkers.forEach(w =>{
-            divzone.innerHTML +=`
+        filterworkers.forEach(w => {
+            divzone.innerHTML += `
             <div class="flex items-center gap-4">
             <img src="${w.image}" class="w-20 h-20 rounded-full object-cover border border-gray-300">
             <div>
@@ -220,7 +220,7 @@ function renderWorker() {
     });
 
     unassignedList.innerHTML = "";
-    filterZoneNull.forEach(membernull=>{
+    filterZoneNull.forEach(membernull => {
         unassignedList.innerHTML += `
             <div class="flex items-center gap-4 unassigneddiv cursor-pointer flex justify-between items-center bg-green-500 border border-gray-200 p-3 rounded-lg" data-id=${membernull.id} >
                 <img src="${membernull.image}" class="w-20 h-20 rounded-full object-cover border border-gray-300" onclick="showProfile(this)">
@@ -237,9 +237,9 @@ function renderWorker() {
 
 }
 
-function restoremember(id){
-    let workerrestore = workers.find(worker=>Number(worker.id) === Number(id))
-    Object.assign(workerrestore,{zoneId:null})
+function restoremember(id) {
+    let workerrestore = workers.find(worker => Number(worker.id) === Number(id))
+    Object.assign(workerrestore, { zoneId: null })
     saveWorkers()
     renderWorker()
 }
@@ -260,43 +260,43 @@ window.addEventListener("DOMContentLoaded", () => {
 //////////////
 // show profile
 function showProfile(card) {
-    const  id = card.closest(".unassigneddiv").dataset.id
-    const filtermemeber = workers.find(worker=>Number(worker.id) === Number(id))
-             let experienceshtml = "";
-             filtermemeber.experiences.forEach(exp => {
-                 experienceshtml += `
-                     <div class="bg-gray-100 p-4 rounded-lg space-y-2">
-                         <p><strong>Company:</strong> ${exp.company}</p>
-                         <p><strong>Role:</strong> ${exp.role}</p>
-                         <p><strong>Period:</strong> ${exp.from} - ${exp.to}</p>
-                     </div>
-                 `;
-             });
-             //injection of html 
-             profileContent.innerHTML = `
-                 <h2 class="text-3xl font-bold text-center mb-6">Profile</h2>
-                 <div class="flex gap-6 items-center">
-                     <img src="${filtermemeber.image}" class="w-32 h-32 rounded-full object-cover border shadow">
-                     <div>
-                         <p class="text-2xl font-bold">${filtermemeber.name}</p>
-                         <p class="text-gray-700 text-lg">${filtermemeber.role}</p>
-                     </div>
-                 </div>
-         
-                 <div class="bg-gray-100 p-4 rounded-lg space-y-2">
-                     <p><strong>Email:</strong> ${filtermemeber.email}</p>
-                     <p><strong>Phone:</strong> ${filtermemeber.phone}</p>
-                 </div>
-         
-                 <h3 class="text-2xl font-bold mt-6">Work Experience :</h3>
-                 ${experienceshtml}
-         
-                 <button class="text-red-700 font-bold text-xl editbtn">Edit</button>
-             `;
-         
-             workerProfile.classList.remove("hidden");
+    const id = card.closest(".unassigneddiv").dataset.id
+    const filtermemeber = workers.find(worker => Number(worker.id) === Number(id))
+    let experienceshtml = "";
+    filtermemeber.experiences.forEach(exp => {
+        experienceshtml += `
+                    <div class="bg-gray-100 p-4 rounded-lg space-y-2">
+                        <p><strong>Company:</strong> ${exp.company}</p>
+                        <p><strong>Role:</strong> ${exp.role}</p>
+                        <p><strong>Period:</strong> ${exp.from} - ${exp.to}</p>
+                    </div>
+                `;
+    });
+    //injection of html 
+    profileContent.innerHTML = `
+                <h2 class="text-3xl font-bold text-center mb-6">Profile</h2>
+                <div class="flex gap-6 items-center">
+                    <img src="${filtermemeber.image}" class="w-32 h-32 rounded-full object-cover border shadow">
+                    <div>
+                        <p class="text-2xl font-bold">${filtermemeber.name}</p>
+                        <p class="text-gray-700 text-lg">${filtermemeber.role}</p>
+                    </div>
+                </div>
         
+                <div class="bg-gray-100 p-4 rounded-lg space-y-2">
+                    <p><strong>Email:</strong> ${filtermemeber.email}</p>
+                    <p><strong>Phone:</strong> ${filtermemeber.phone}</p>
+                </div>
         
+                <h3 class="text-2xl font-bold mt-6">Work Experience :</h3>
+                ${experienceshtml}
+        
+                <button class="text-red-700 font-bold text-xl editbtn">Edit</button>
+            `;
+
+    workerProfile.classList.remove("hidden");
+
+
 }
 
 
@@ -363,17 +363,17 @@ const zoneRules = {
     "security-room": ["security", "manager"],
     "archives-room": ["manager"],
     "staff-room": ["manager", "cleaning"],
-    "conference-room": ["manager","cleaning","security","itguy","receptionest"]
+    "conference-room": ["manager", "cleaning", "security", "itguy", "receptionest"]
 };
 
-
+//open pop up for each
 document.querySelectorAll(".add-btn").forEach(btn => {
     const assignWorkersList = document.getElementById("assignWorkersList");
     btn.addEventListener("click", () => {
         let memberzone = [];
         currentZone = btn.dataset.zone;
-        zoneRules[currentZone].forEach(cur=>{
-            memberzone.push(...workers.filter(worker=>worker.role === cur && worker.zoneId === null));
+        zoneRules[currentZone].forEach(cur => {
+            memberzone.push(...workers.filter(worker => worker.role === cur && worker.zoneId === null));
         })
         console.log(assignWorkersList)
         assignWorkersList.innerHTML = "";
@@ -387,12 +387,12 @@ document.querySelectorAll(".add-btn").forEach(btn => {
                 workerBtn.innerHTML = `<div data-id=${worker.id} class ="bg-white p-8 w-11/12 md:w-3/4 max-w-3xl rounded shadow-lg relative max-h-[90vh] overflow-y-auto ">
                 <p> Name: ${worker.name} Role: (${worker.role})</p>
                 </div>`
-            workerBtn.addEventListener("click", () => {
-                    const memberselected = workers.find(worker=>Number(worker.id) === Number(workerBtn.firstElementChild.dataset.id))
-                    Object.assign(memberselected,{zoneId:currentZone})
+                workerBtn.addEventListener("click", () => {
+                    const memberselected = workers.find(worker => Number(worker.id) === Number(workerBtn.firstElementChild.dataset.id))
+                    Object.assign(memberselected, { zoneId: currentZone })
                     saveWorkers()
                     renderWorker()
-                            assignPopup.classList.add("hidden");
+                    assignPopup.classList.add("hidden");
 
                 });
                 assignWorkersList.appendChild(workerBtn);
@@ -400,8 +400,9 @@ document.querySelectorAll(".add-btn").forEach(btn => {
         }
 
         assignPopup.classList.remove("hidden");
-        
-    })})
+
+    })
+})
 // Close popup
 assignCancel.addEventListener("click", () => assignPopup.classList.add("hidden"));
 assignPopup.addEventListener("click", e => {
@@ -411,3 +412,24 @@ assignPopup.addEventListener("click", e => {
 
 
 renderWorker()
+
+
+//Unassigned All workers btn
+
+const unassignAllBtn = document.getElementById("unassignAllBtn");
+
+if (unassignAllBtn) {
+    unassignAllBtn.addEventListener("click", () => {
+
+        if (!confirm("Are you sure ? All workers will be unassigned")) 
+            return;
+        workers.forEach(worker => {
+            worker.zoneId = null;
+        });
+
+        saveWorkers();
+        renderWorker();
+
+        alert("Successfully Unassigned ");
+    });
+}
